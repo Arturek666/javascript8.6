4c5064ba7937bc36eaa00b2f97d55ccc40baf277
Artur Schulz <artur.schulz123@gmail.com>
2018-02-22 10:46:03 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var a = 5;
+var b = 6;
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log(value);
+if (value > 0) {
+    console.log('Wynik dodatni');
+}
+else if (value < 0) {
+    console.log('Wynik ujemny');
+}
+else {
+    console.log('Wynik równy 0');
+}
